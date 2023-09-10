62615f9af89e357847fa53126c6ae6180498a6bd
Juan Eduardo <89925656+johnedwars@users.noreply.github.com>
2023-09-10 16:16:35 -0400

    Add files via upload

---
+++ src/todos/use-cases/create-todo-html.js
@@ -0,0 +1,26 @@
+import { Todo } from "../models/todoModels";
+
+
+
+export const createTodoHtml = (todo) => {
+    if (!todo) throw new Error ('A todo object is required');
+
+    const html = `
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${todo.done ? 'checked': '' }>
+                    <label>${todo.description}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+           `;
+
+    const liElement = document.createElement('li');
+    liElement.innerHTML = html;
+    liElement.setAttribute('data-id', todo.id);
+
+    if (todo.done)
+    liElement.classList.add('completed');
+
+
+    return liElement;
+}
